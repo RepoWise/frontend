eae8691f060de41318ee0a71f0729d7c2ff9a190
Nafiz Imtiaz Khan <43680716+Nafiz43@users.noreply.github.com>
2025-12-03 12:11:15 -0800

    Enhance RepoWise response formatting

--- src/components/ChatInterface.jsx
+++ src/components/ChatInterface.jsx
@@ -38,6 +38,7 @@ import { ThemeToggle } from './ThemeToggle'
 import { useAuth } from '../contexts/AuthContext'
 import { highlightEntities } from '../lib/highlightEntities'
 import { highlightSustainabilityTerms } from '../lib/sustainabilityHighlights'
+import { formatResponseLayout } from '../lib/responseFormatter'
 
 const EXAMPLE_REPOSITORIES = [
   {
@@ -297,7 +298,8 @@ function ChatInterface() {
       const uniqueSources = Object.values(sourcesByPath)
 
       const rawResponse = data?.data?.response || 'No response received'
-      const entityHighlighted = highlightEntities(rawResponse)
+      const structuredResponse = formatResponseLayout(rawResponse)
+      const entityHighlighted = highlightEntities(structuredResponse)
       const styledResponse = highlightSustainabilityTerms(entityHighlighted)
 
       setMessages(prev => [...prev, {
